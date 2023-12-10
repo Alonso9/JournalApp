541b1dd81f1a1cf709c7ec403f5780c7d2bdb6d4
Alonso9 <ramonalonso_giron@ucol.mx>
2023-12-09 21:19:40 -0600

    Se intallo react router dom 6

---
+++ src/router/AppRputer.jsx
@@ -0,0 +1,13 @@
+import {Routes, Route} from 'react-router-dom'
+
+export const AppRputer = () => {
+  return (
+    <>
+     <Routes>
+        Login y registro
+       <Route/>
+       <Route/>
+    </Routes>   
+    </>
+  )
+}
